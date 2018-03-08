c36af3c87e5e2ec73266ba19b46d63f6167ef370
Saige Leslie <saigeleslie@gmail.com>
2018-03-08 00:13:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ function destructivelyRemoveFirstKitten(){
   return kittens;
 }
 function appendKitten(name){
-  newKittens = [...kittens , name];
-  return newKittens;
+  newArray = [...kittens , name];
+  return newArray;
 }
